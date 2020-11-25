40dde636d9cc99f7401d55fb059a83fb9345e30a
Dronandre <dronandre@yandex.ru>
2020-11-26 00:11:48 +0700

    Get data from API

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+	return (
+		<div class="loadingio-spinner-spin-hj2e76b286t">
+			<div class="ldio-2xkr1jfdtd9">
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Spinner;
