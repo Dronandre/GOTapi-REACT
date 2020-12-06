c90ec3680cf99942db19547bcd85665fc00538b6
Dronandre <dronandre@yandex.ru>
2020-12-06 17:23:37 +0700

    Add component page and render error

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,9 +2,8 @@ import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
-import ErrorMessage from '../errorMessage';
+import ErrorMessage from '../error';
+import CharacterPage from '../characterPage';
 
 import './app.css';
 
@@ -21,6 +20,7 @@ export default class App extends Component {
             }
         });
     }
+
     render() {
         if (this.state.error) {
             return <ErrorMessage/>
@@ -40,14 +40,7 @@ export default class App extends Component {
                                 onClick={this.toggleRandomChar}>Toggle random character</button>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col md='6'>
-                            <ItemList />
-                        </Col>
-                        <Col md='6'>
-                            <CharDetails />
-                        </Col>
-                    </Row>
+                    <CharacterPage/>
                 </Container>
             </>
         );
