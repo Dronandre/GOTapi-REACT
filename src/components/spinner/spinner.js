0960dbbdfa4c4efc37e7207cad9f7fa6e0c02af9
Dronandre <dronandre@yandex.ru>
2020-12-14 23:21:18 +0700

    react router

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,58 +1,12 @@
-import React from "react";
-import "./spinner.css";
+import React from 'react';
+import './spinner.css';
 
 const Spinner = () => {
-	return (
-		<div class="loadingio-spinner-spin-hj2e76b286t">
-			<div class="ldio-2xkr1jfdtd9">
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-				<div>
-					<div />
-				</div>
-			</div>
-		</div>
-	);
-};
+    return (
+        <div className="lds-css ng-scope">
+            <div className="lds-spin"><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div></div>
+        </div>
+    )
+}
 
 export default Spinner;
